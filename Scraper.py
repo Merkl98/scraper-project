8e8d9aaf2734e138601d2798f73d688333b93f09
Alex Merkl <alexmerkl50@gmail.com>
2026-05-05 06:39:15 -0700

    feat: output file sorted by price

--- Scraper.py
+++ Scraper.py
@@ -1,18 +1,9 @@
-# HTTP requests
 import requests
-
-# HTML parsing
 from bs4 import BeautifulSoup
-
-# data handling (useful later when saving results)
 import pandas as pd
-
-# regex for extracting numbers, prices, etc.
 import re
 
 from collections import namedtuple 
-
-# optional typing (helps readability)
 from typing import List, Dict, Optional
 
 AUCTION_PAGE_URL = "https://urbanauctions.ca/"
@@ -20,18 +11,16 @@ WAREHOUSE_URL = "https://www.skatewarehouse.com/Clearance_Skateboard_Clothing/ca
 
 def main() -> None:
     url = WAREHOUSE_URL
-    df = pd.DataFrame
+    df = pd.DataFrame()
 
 #think tree: need to loop each category section with nested loops for headers - items.
-
 
     response = get_web_data(url)
     soup = BeautifulSoup(response.text, "html.parser")
     containers = soup.find_all("a", class_="cattable-wrap-cell-info")
-    articles = soup.find_all("div", class_="catheader")
-    print(articles)
 
-    df = load_pandas(containers, df, articles)
+    df = load_pandas(containers, df)
+    df = price_ascending(df)
     print_csv(df)
 
 def get_web_data(url):
@@ -39,20 +28,25 @@ def get_web_data(url):
     response.raise_for_status()
     return response
 
-def load_pandas(items, data, type):
+def load_pandas(items, data):
     clothes = []
-    article = type.find("h2").text.strip()
     for container in items:
         name = container.find("div", class_="cattable-wrap-cell-info-name").text.strip()
         link = container["href"]
-        price = container.find("div", class_="cattable-wrap-cell-info-price").find("span").text.strip()
-        item = {"Name": name, "Link": link, "Price": price, "Type": article}
+        price = container.find("div", class_="cattable-wrap-cell-info-price").find("span").text.strip().replace("$","")
+        price = float(price)
+        item = {"Name": name, "Link": link, "Price": price}
         clothes.append(item)
     data = pd.DataFrame(clothes)
     return data
 
 def print_csv(data):
     data.to_csv("clearance_findings.csv", index=False)
+
+def price_ascending(data):
+    data = data.sort_values("Price")
+    print(data)
+    return data
 
 
 if __name__ == "__main__":
